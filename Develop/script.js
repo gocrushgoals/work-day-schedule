b7e5959606eeb38d2ed58fc3ee82d8a131de342d
gocrushgoals <gocrushgoals@gmail.com>
2024-03-10 21:04:49 -0400

    Save button event listener

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,14 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
-
-
+$(document).ready(function() {
+  console.log("Ready");
+});
+    //Display current date and time (Moment.js)
+    let now = moment().format("dddd, MMMM Do YYYY");
+    let displayDate = document.getElementById("currentDay");
+    displayDate.innerHTML = now;
+    let currentHour = moment().format("HH");
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -12,7 +17,14 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+
+  $(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var value = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time,value);
+});
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
